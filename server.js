0778ee6f1c0471cd6fc19aab9ca099b702329160
Guichard <rene.guichard7@gmail.com>
2025-10-24 01:34:21 +0200

    Vers2

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
-const { createServer } = require('node:http');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+http.createServer((req, res) => {
+  if (req.method === 'GET' && req.url === '/') {
+    const filePath = path.join(__dirname, 'index.html');
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Internal Server Error');
+        return;
+      }
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      res.end(data);
+    });
+  
 
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not Found');
+  }
+}).listen(3000, () => {''
+  console.log('Server running at http://localhost:3000/');
 });
